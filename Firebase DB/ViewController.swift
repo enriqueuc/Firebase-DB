291df75874eef39f368820e598e9327c4c8e5280
Ing. Enrique Ugalde <eugalde@ing-enrique-ugalde-macbook-pro.local>
2016-07-24 15:01:36 -0500

    with loading and retrieving data from fb

--- Firebase DB/ViewController.swift
+++ Firebase DB/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 import Firebase
 import FirebaseDatabase
 
-class TableViewController: UITableViewController {
+//define struct of post
+struct postStructure {
+    let title : String!
+    let message : String!
+}
+
+class ViewController: UITableViewController {
+    
+    //to work with the post
+    var posts = [postStructure]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //load data from DB
+        let databaseRef = FIRDatabase.database().reference()
+        databaseRef.child("Posts").queryOrderedByKey().observeEventType(.ChildAdded, withBlock: {
+            snapshot in
+            
+            let title = snapshot.value!["title"] as! String
+            let message = snapshot.value!["message"] as! String
+            
+            self.posts.insert(postStructure(title: title, message: message), atIndex: 0)
+            
+            self.tableView.reloadData()
+                })
+        
         post()
     }
     
@@ -22,7 +45,7 @@ class TableViewController: UITableViewController {
         
         let title = "Título"
         let message = "Message"
-         
+        
         //upload to DB
         
         let post : [String : AnyObject] = ["title" : title,
@@ -40,6 +63,19 @@ class TableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("Cell")
+        let label1 = cell?.viewWithTag(1) as! UILabel
+        label1.text = posts[indexPath.row].title
+        
+        let label2 = cell?.viewWithTag(2) as! UILabel
+        label2.text = posts[indexPath.row].message
+        
+        return cell!
+    }
 }
 
